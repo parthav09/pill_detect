9a0fc1e32fb27a919c3a1546952f9369d5540502
Parthav Joshi <parthavjoshi1718@gmail.com>
2022-10-09 17:10:19 +0530

    Added new image

--- contours_edges/identify.py
+++ contours_edges/identify.py
@@ -1,20 +1,19 @@
 import cv2
-import numpy as np
 import time
 from argparse import ArgumentParser
 import imutils
 
 # Using command line arguments for importing mages
 z=0
-start = time.time()
+start = time.time() # starting the timer to time the process
 
 ap = ArgumentParser()
 ap.add_argument("--pills", "-i", help="Path to the image", required=True)
 args = vars(ap.parse_args())
 
-pills = cv2.imread(args["pills"])
-gray = cv2.cvtColor(pills, cv2.COLOR_BGR2GRAY)
-# blurred = cv2.GaussianBlur(gray, (9,9), 0)
+pills = cv2.imread(args["pills"]) # reading the image
+gray = cv2.cvtColor(pills, cv2.COLOR_BGR2GRAY) # convert the image to a grayscale image
+# blurred = cv2.GaussianBlur(gray, (9,9), 0) # blurring
 
 # using thresholding to convert to binary
 (T, threshinv) = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
@@ -23,11 +22,11 @@ gray = cv2.cvtColor(pills, cv2.COLOR_BGR2GRAY)
 xaxis = cv2.Sobel(threshinv, ddepth=cv2.CV_64F, dx=1, dy=0)  # identifying horizontal changes
 yaxis = cv2.Sobel(threshinv, ddepth=cv2.CV_64F, dx=0, dy=1)  # identifying vertical changes
 
-# calculating the absilute value of gradient images
+# calculating the absolute value of gradient images
 xaxis = cv2.convertScaleAbs(xaxis)
 yaxis = cv2.convertScaleAbs(yaxis)
 
-combinedpills = cv2.addWeighted(xaxis, 0.5, yaxis, 0.5, 0)
+combinedpills = cv2.addWeighted(xaxis, 0.5, yaxis, 0.5, 0)# Gx + Gy
 
 cv2.imshow('Combined: ', combinedpills)  # combined images
 cv2.imshow("Threshed: ", threshinv)  # thresholded images
@@ -36,6 +35,7 @@ cv2.imshow("Threshed: ", threshinv)  # thresholded images
 contours = cv2.findContours(combinedpills.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 contours = imutils.grab_contours(contours)
 print(len(contours))
+
 # removing unnesecary contours
 for (i, c) in enumerate(contours):
     area = cv2.contourArea(c)
@@ -45,7 +45,8 @@ for (i, c) in enumerate(contours):
         (x, y, w, h) = cv2.boundingRect(c)
         (centerx, centery) = (x + (w // 2), y + (h // 2))
         (b, g, r) = pills[centery, centerx]
-        if (r > 200):
+        print(b,g,r)
+        if (r > 200 and b <50 and g>200):
             cv2.drawContours(pills, c, -1, (0, 0, 255), 2)
         else:
             cv2.drawContours(pills, c, -1, (0, 225, 0), 2)
--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-print("None")
+import numpy as np
+import streamlit as st
+import cv2
+
+uploaded_file = st.file_uploader("Choose a image file", type="bmp")
+
+if uploaded_file is not None:
+    # Convert the file to an opencv image.
+    file_bytes = np.asarray(bytearray(uploaded_file.read()), dtype=np.uint8)
+    opencv_image = cv2.imdecode(file_bytes, 1)
+
+    # Now do something with the image! For example, let's display it:
+    st.image(opencv_image, channels="BGR")
+    gray = cv2.cvtColor(opencv_image, cv2.COLOR_BGR2GRAY)
+    # st.image(gray)
+    (T, threshinv) = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
+    xaxis = cv2.Sobel(threshinv, ddepth=cv2.CV_64F, dx=1, dy=0)  # identifying horizontal changes
+    yaxis = cv2.Sobel(threshinv, ddepth=cv2.CV_64F, dx=0, dy=1)  # identifying vertical changes
